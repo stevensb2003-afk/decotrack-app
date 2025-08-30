f9c1aa4cb3eb5b49f6cc963660c4fe5e94d4db1e
Steven Sánchez Bonilla <StevenSB.2003@gmail.com>
2025-08-30 19:05:03 +0000

    Also images names are "Logo Azul" and "Logo Blanco" for the main ones an

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/Favicon%20Azul.png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
