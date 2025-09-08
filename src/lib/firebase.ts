f5594e20dd107abe83371b29286438691fa32166
Steven Sánchez Bonilla <StevenSB.2003@gmail.com>
2025-09-08 20:30:36 +0000

    pero para explicar, desde la consola de firebase activé un App Hosting y

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,21 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// This configuration is safe to be public.
 const firebaseConfig = {
   "projectId": "decotrack-l9y8l",
   "appId": "1:602878187874:web:95bbedd4e76e455e99f43b",
   "storageBucket": "decotrack-l9y8l.appspot.com",
-  "apiKey": "AIzaSyDlDmioqDC8-i0aP61SSk4AJAixssy9ax0",
+  "apiKey": process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   "authDomain": "decotrack-l9y8l.firebaseapp.com",
   "messagingSenderId": "602878187874"
 };
 
-// Initialize Firebase
+// Initialize Firebase for SSR and SSG
+// This ensures we don't initialize the app more than once.
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
